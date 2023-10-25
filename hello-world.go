b5fe3fa7f8c85f8ec688d48578896d457b026fbb
Jason Ray <jayray.net@gmail.com>
2023-10-25 19:57:19 -0400

    use variables

--- hello-world.go
+++ hello-world.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("hello world")
+    var greeting = "hello world!"
+	fmt.Println(greeting)
+
+    fmt.Println("go" + "lang")
+
+	var x = 1
+	var y = 2
+	var sum = x + y
+    fmt.Println("1+2 =", sum)
+
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
